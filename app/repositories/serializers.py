fce42e1eb305945c83c682cb45f6ae3d9dca2273
slucero10 <72504459+slucero10@users.noreply.github.com>
2022-10-16 23:28:15 -0500

    [PIZ2] Add a beverage model manager and service BE (#5)
    * feat: beverage model, manager and service added
    
    * test: test for beverages added

--- app/repositories/serializers.py
+++ app/repositories/serializers.py
@@ -1,5 +1,5 @@
 from app.plugins import ma
-from .models import Ingredient, Size, Order, OrderDetail
+from .models import Ingredient, Size, Beverage, Order, OrderDetail, BeverageDetail
 
 
 class IngredientSerializer(ma.SQLAlchemyAutoSchema):
@@ -17,6 +17,13 @@ class SizeSerializer(ma.SQLAlchemyAutoSchema):
         load_instance = True
         fields = ('_id', 'name', 'price')
 
+class BeverageSerializer(ma.SQLAlchemyAutoSchema):
+
+    class Meta:
+        model = Beverage
+        load_instance = True
+        fields = ('_id', 'name', 'size', 'price')
+
 
 class OrderDetailSerializer(ma.SQLAlchemyAutoSchema):
 
@@ -31,9 +38,23 @@ class OrderDetailSerializer(ma.SQLAlchemyAutoSchema):
         )
 
 
+class BeverageDetailSerializer(ma.SQLAlchemyAutoSchema):
+
+    beverage = ma.Nested(BeverageSerializer)
+
+    class Meta:
+        model = BeverageDetail
+        load_instance = True
+        fields = (
+            'beverage_price',
+            'beverage'
+        )
+
+
 class OrderSerializer(ma.SQLAlchemyAutoSchema):
     size = ma.Nested(SizeSerializer)
     detail = ma.Nested(OrderDetailSerializer, many=True)
+    beverage_detail = ma.Nested(BeverageDetailSerializer, many=True)
 
     class Meta:
         model = Order
@@ -47,5 +68,6 @@ class OrderSerializer(ma.SQLAlchemyAutoSchema):
             'date',
             'total_price',
             'size',
-            'detail'
+            'detail',
+            'beverage_detail'
         )
