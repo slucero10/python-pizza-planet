26a8a8b8b00477392af9d2b1737736445821a714
slucero10 <72504459+slucero10@users.noreply.github.com>
2022-10-17 01:31:36 -0500

    [PIZ3] Design patterns

---
+++ app/adapters/ingredient.py
@@ -0,0 +1,15 @@
+from typing import List
+
+from ..repositories.models import Ingredient
+
+class IngredientAdapter:
+    def get_ingredients_from_txt(file_path: str) -> List[Ingredient]:
+        try:
+            with open(file_path, 'r') as file:
+                ingredients = []
+                while (line := file.readline().rstrip()):
+                    line_arr = line.split(';')
+                    ingredients.append(Ingredient(name=line_arr[0], price=float(line_arr[2])))
+                return ingredients
+        except Exception as e:
+            raise Exception(str(e))
---
+++ app/adapters/size.py
@@ -0,0 +1,15 @@
+from typing import List
+
+from ..repositories.models import Size
+
+class SizeAdapter:
+    def get_sizes_from_txt(file_path: str) -> List[Size]:
+        try:
+            with open(file_path, 'r') as file:
+                sizes = []
+                while (line := file.readline().rstrip()):
+                    line_arr = line.split(';')
+                    sizes.append(Size(name=line_arr[0], price=float(line_arr[2])))
+                return sizes
+        except Exception as e:
+            raise Exception(str(e))
